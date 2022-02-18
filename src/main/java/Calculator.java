cea027f7bf5cb241f72762b56821b303ac460e72
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-02-18 11:46:53 +0000

    add Calculator and method

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+
+    public static int multiply(int x, int y) {
+        return (x*y);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
+        int result = Calculator.multiply(3,5);
+        System.out.println("result = " + result);
     }
 }
